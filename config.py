fe552ed134b54393f041bbeec6d8031fd07a3429
Tryggve Klevstul-Jensen <tryggvekj@gmail.com>
2026-03-21 17:00:18 +0100

    make workplace structure

---
+++ config.py
@@ -0,0 +1,8 @@
+# External data paths
+PATH_TO_DATA_FOLDER = "~/cluster/projects/vc/courses/TDT17/ad/Poles2025"
+
+FOLDER_IPHONE = "Road_poles_iPhone"
+TRAIN_IMAGES_IPHONE = "Train.txt"
+VALIDATION_IMAGES_IPHONE = "Train.txt"
+
+FOLDER_v1 = "roadpoles_v1"
