231ba9a7cd5341c8f9393fecba85a2d098fa1281
Tryggve Klevstul-Jensen <tryggvekj@gmail.com>
2026-03-22 21:11:53 +0100

    feat: add yolo prediction for test set

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from PIL import Image
+from ultralytics import YOLO
+import config
 
-# Single image
-img = Image.open("data/data_iphone_yolo/images/train/919135364.jpg")
-width, height = img.size
-print(f"Image size: {width}x{height}")
+WEIGHTS_PATH = "models/weights/yolo/v2.pt"
+SOURCE_PATH = str(config.DATA_FOLDER / config.FOLDER_IPHONE / "images/Test/test")
+SAVE_FOLDER = "results_iphone"
+SUB_SAVE_FOLDER = "predict"
+DEVICE = "cpu"
+
+
+def main() -> None:
+	
+    # YOLO prediction
+	model = YOLO(WEIGHTS_PATH)
+	model.predict(
+		source=SOURCE_PATH,
+		project=SAVE_FOLDER,
+		name=SUB_SAVE_FOLDER,
+		device=DEVICE,
+		save=True,
+		save_txt=True,
+		save_conf=True,
+	)
+
+	print(f"Saved predictions to: {SAVE_FOLDER}/{SUB_SAVE_FOLDER}")
+
+
+if __name__ == "__main__":
+	main()
