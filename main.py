9271d2c573606e713c5610fc7878088eb84ac406
Tryggve Klevstul-Jensen <tryggvekj@gmail.com>
2026-03-26 13:15:57 +0100

    add new parameter that reduces overlapping prediction boxes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from ultralytics import YOLO
 import config
 
-WEIGHTS_PATH = "models/weights/yolo/v2.pt"
-SOURCE_PATH = str(config.DATA_FOLDER / config.FOLDER_IPHONE / "images/Test/test")
-SAVE_FOLDER = "results_iphone"
+WEIGHTS_PATH = "best_optuna_weights.pt"
+SOURCE_PATH = str(config.DATA_FOLDER / config.FOLDER_V1 / "test/images")
+SAVE_FOLDER = "v1"
 SUB_SAVE_FOLDER = "predict"
+# WEIGHTS_PATH = "models/yolo/weights/iphone/v5.pt"
+# SOURCE_PATH = str(config.DATA_FOLDER / config.FOLDER_IPHONE / "images/Test/test")
+# SAVE_FOLDER = "iphone"
+# SUB_SAVE_FOLDER = "predict"
 DEVICE = "cpu"
 
 
 def main() -> None:
 	
-    # YOLO prediction
+    # YOLO prediction	
 	model = YOLO(WEIGHTS_PATH)
 	model.predict(
 		source=SOURCE_PATH,
@@ -20,6 +24,7 @@ def main() -> None:
 		save=True,
 		save_txt=True,
 		save_conf=True,
+		iou=0.2, # Set NMS to lower such that we don't get overlapping boxes if they are overlapping with more than 20%
 	)
 
 	print(f"Saved predictions to: {SAVE_FOLDER}/{SUB_SAVE_FOLDER}")
